50ee4513f5391ba906e5883bccc5288713cb667b
Lalavath-Akhil <156202091+Lalavath-Akhil@users.noreply.github.com>
2024-06-05 12:23:42 +0530

    first commit

---
+++ src/main/java/ClickCounterTestPage.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClickCounterTestPage {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\hi\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qaclickcounter.ccbp.tech/");
+
+
+        WebElement clickButton = driver.findElement(By.xpath("//button[contains(text(), 'Click Me!')]"));
+
+
+        WebElement counter = driver.findElement(By.xpath("//span[contains(@class, 'counter')]"));
+
+        for (int i = 1; i <= 100; i++) {
+
+
+            clickButton.click();
+
+
+            String countStr = counter.getText();
+            int count = Integer.parseInt(countStr);
+
+            if (count != i) {
+                System.out.println("Count Mismatch");
+                break;
+            }
+            if(i == 100) {
+                System.out.println("Click Counter App: Working as expected");
+            }
+        }
+
+
+        driver.quit();
+    }
+}
